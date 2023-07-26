85293bd91318f7e41b5f36e6f4d78b35bff0fd88
Anyikabs <114746509+Anyikabs@users.noreply.github.com>
2023-07-26 04:37:52 +0100

    6-completed_tasks.js fixed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,45 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const getCompletedTasksByUser = (apiUrl) => {
-  return new Promise((resolve, reject) => {
-    request(apiUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-        return;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-
-      if (response.statusCode !== 200) {
-        reject(`Request failed with status code ${response.statusCode}`);
-        return;
-      }
-
-      const tasks = JSON.parse(body);
-      const completedTasksByUser = {};
-
-      tasks.forEach((task) => {
-        if (task.completed) {
-          completedTasksByUser[task.userId] = (completedTasksByUser[task.userId] || 0) + 1;
-        }
-      });
-
-      resolve(completedTasksByUser);
     });
-  });
-};
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node 6-completed_tasks.js <API_URL>');
-} else {
-  const apiUrl = process.argv[2];
-
-  getCompletedTasksByUser(apiUrl)
-    .then((completedTasks) => {
-      console.log(completedTasks);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
-
+    console.log(completed);
+  }
+});
